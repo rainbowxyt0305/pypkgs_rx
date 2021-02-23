fb49801c94d24925b54f9690e35cf1f545719ff0
Rachel Xu <rachelxu.sauderfinance@gmail.com>
2021-02-23 12:24:43 -0800

    initial package setup

---
+++ docs/usage.rst
@@ -0,0 +1,7 @@
+=====
+Usage
+=====
+
+To use pypkgs_RX in a project::
+
+    import pypkgs_rx
---
+++ tests/test_pypkgs_rx.py
@@ -0,0 +1,5 @@
+from pypkgs_rx import __version__
+from pypkgs_rx import pypkgs_rx
+
+def test_version():
+    assert __version__ == '0.1.0'
