0320964fd24eea59abc2281c7cda0038f00d0592
Rachel Xu <rachelxu.sauderfinance@gmail.com>
2021-02-23 22:43:10 -0800

    added unit tests for catbind

--- tests/test_pypkgs_rx.py
+++ tests/test_pypkgs_rx.py
@@ -1,5 +1,17 @@
 from pypkgs_rx import __version__
 from pypkgs_rx import pypkgs_rx
+import pandas as pd
+
 
 def test_version():
     assert __version__ == '0.1.0'
+
+
+def test_catbind():
+    a = pd.Categorical(["character", "hits", "your", "eyeballs"])
+    b = pd.Categorical(["but", "integer", "where it", "counts"])
+    assert ((pypkgs_rx.catbind(a, b)).codes == [1, 4, 7, 3, 0, 5, 6, 2]).all()
+    assert ((pypkgs_rx.catbind(a, b)).categories == [
+        "but", "character", "counts", "eyeballs", "hits", "integer",
+        "where it", "your"
+    ]).all()
